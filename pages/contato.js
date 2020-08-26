1056445cae2a2a66e8993e201f717b6c9076add2
Matheus Mendes Pedro Peres <matheusmendespedroperes2@gmail.com>
2020-08-26 07:56:24 -0300

    .

--- pages/contato.js
+++ pages/contato.js
@@ -8,9 +8,7 @@ const Contato = () => {
       <PageTitle title='Contato' />
       <h1>Contato</h1>
       <div>
-        <Link href='/'>
-          <a>Home</a>
-        </Link>
+        <a className='bg-blue-400 px-12 py-4 font-bold rounded-lg shadow-lg hover:shadow'>Entrar em contato</a>
       </div>
     </div>
   )
