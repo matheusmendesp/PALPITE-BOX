73b241aecccef47d3c3f020b6d696f6c63bfd039
Matheus Mendes Pedro Peres <matheusmendespedroperes2@gmail.com>
2020-08-26 08:01:29 -0300

    Atualizando contato

--- pages/contato.js
+++ pages/contato.js
@@ -6,9 +6,9 @@ const Contato = () => {
   return (
     <div>
       <PageTitle title='Contato' />
-      <h1>Contato</h1>
-      <div>
-        <a className='bg-blue-400 px-12 py-4 font-bold rounded-lg shadow-lg hover:shadow'>Entrar em contato</a>
+      <p className='text-center my-12'>Para entrar em contato clique no botão a baixo</p>
+      <div className='text-center my-12'>
+        <a className='bg-blue-400 px-12 py-4 font-bold rounded-lg shadow-lg hover:shadow' href='https://api.whatsapp.com/send?phone=5514998320338' >Entrar em contato</a>
       </div>
     </div>
   )
