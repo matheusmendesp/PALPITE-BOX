f219f62b9c45a810c11a85d4d428ff6549ac4571
Matheus Mendes Pedro Peres <matheusmendespedroperes2@gmail.com>
2021-01-08 14:11:34 -0300

    aaa

--- components/Header/index.js
+++ components/Header/index.js
@@ -13,12 +13,6 @@ const Header = () => {
         </div>
       </div>
       <div className='bg-gray-300 p-4 shadow-md text-center'>
-        <Link href='/sobre'>
-          <a className='px-2 hover:underline'>Sobre</a>
-        </Link>
-        <Link href='/contato'>
-          <a className='px-2 hover:underline'>Contato</a>
-        </Link>
         <Link href='/pesquisa'>
           <a className='px-2 hover:underline'>Pesquisa</a>
         </Link>
