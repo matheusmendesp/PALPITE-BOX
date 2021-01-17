afe01acc6148b9e43b7f601cf50645ac537560c2
matheusmendesp <61604854+matheusmendesp@users.noreply.github.com>
2021-01-17 09:31:37 -0300

    Update assistir.js

--- pages/assistir.js
+++ pages/assistir.js
@@ -6,7 +6,7 @@ const Assistir = () => {
             <p className='mt-12 text-center'>
                 <strong>Vagas esgotadas para o dia que você queria?</strong>
                 <br/><br/>
-                Não tem problema, assista o culto online a baixo. Curta, comente e compartilhe!
+                Não tem problema, assista o culto online abaixo. Curta, comente e compartilhe!
             </p>
             <div className='my-12'>
                 <iframe className='mt-12 mx-auto' width="560" height="315" src="https://www.youtube.com/embed/rxufLvdJCs4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
