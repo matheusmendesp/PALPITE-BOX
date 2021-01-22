58b19ffae5303066ff3f91c30af87eebb1b5e24e
matheusmendesp <61604854+matheusmendesp@users.noreply.github.com>
2021-01-22 16:44:18 -0300

    Update assistir.js

--- pages/assistir.js
+++ pages/assistir.js
@@ -9,7 +9,7 @@ const Assistir = () => {
                 Não tem problema, assista o culto online abaixo. Curta, comente e compartilhe!
             </p>
             <div className='my-12'>
-                <iframe className='mt-12 mx-auto' width="560" height="315" src="https://www.youtube.com/embed/qEG8afrXIh8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe className='mt-12 mx-auto' width="560" height="315" src="https://www.youtube.com/embed/n4YzQ0GUQq8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
         </div>
     )
